177574bd715f3907a9cce74048cefdd85de93330
2262905133 <2262905133@qq.com>
2014-08-22 11:36:33 +0800

    Disney 2014-8-22 迪士尼中国完成
    迪士尼中国完成

---
+++ disney/js/adv.js
@@ -0,0 +1,19 @@
+document.write('<div id="slides">
+					<div class="slides_container" name="adv_28">
+						<div class="slide">
+							<a  target="_blank" idx="1" advid="28" class="adv_link" href="#">
+								<img border="0" hspace="0" alt="" src="images1406103076083.jpg" />
+							</a>
+						</div>
+						<div class="slide">
+							<a  target="_blank" idx="2" advid="28" class="adv_link" href="#">
+								<img border="0" hspace="0" alt="" src="images1406888950724.jpg" />
+							</a>
+						</div>
+						<div class="slide">
+							<a  target="_blank" idx="3" advid="28" class="adv_link" href="#">
+								<img border="0" hspace="0" alt="" src="images/1394437506984.jpg" />
+							</a>
+						</div>
+					</div>
+				</div>');
--- disney/js/index.js
+++ disney/js/index.js
@@ -6,4 +6,40 @@ $(function(){
 		$(this).find(".my_search_down").hide();
 	});
 
+	$(".subnav_ul li").hover(function(){
+		$(this).find(".subnav_down").show();		
+	},function(){
+		$(this).find(".subnav_down").hide();
+	});
+
+	$(".tab1_li a").hover(function(){
+		$(this).find(".tab_li_img").hide();
+		$(this).find(".tab_li_img_0n").show();
+	},function(){
+		$(this).find(".tab_li_img").show();
+		$(this).find(".tab_li_img_0n").hide();
+	});
+
+	tabPrevNext();
+
 });
+
+function tabPrevNext(){
+
+	$(".next").click(function(){
+		var Left = Math.floor($(".tab1_ul").attr("left")) ;		
+		Left = Left-170;		
+		$(".tab1_ul").css("left",''+ Left +'px');		
+		$(".tab1_ul").attr("left",''+ Left +'');
+
+	});
+	
+	$(".prev").click(function(){
+		var Left = Math.floor($(".tab1_ul").attr("left")) ;			
+		Left = Left+170;		
+		$(".tab1_ul").css("left",''+ Left +'px');		
+		$(".tab1_ul").attr("left",''+ Left +'');
+	});
+
+}
+
